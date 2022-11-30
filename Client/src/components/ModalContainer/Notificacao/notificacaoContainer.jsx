ecd4edbf3a98a587c1c94eb0a3937c753175e327
giuliafranca <82042116+giuliafranca@users.noreply.github.com>
2022-11-30 00:12:48 -0300

    tinha esquecido de um bagulhinho
    (segue horripilante)

--- Client/src/components/ModalContainer/Notificacao/notificacaoContainer.jsx
+++ Client/src/components/ModalContainer/Notificacao/notificacaoContainer.jsx
@@ -6,8 +6,7 @@ function NotificacaoContainer(props) {
         <>
             {props.detalhes.map((valor, indice)=>
                 <div className="notificacao" key={indice}>
-                    <p className="noticacao-box">{valor.titulo}</p>
-                    <p>{valor.alteracao}</p>
+                    <p className="noticacao-box"> O compo {valor.titulo} foi atualizado para  {valor.alteracao}</p>
                 </div>
             )}
         </>
