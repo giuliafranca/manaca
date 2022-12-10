0dc6a4efec01fbf143ff2992a3659cbd78fb79bd
Arthur Sant'Anna <84470146+Arthur-SantAnna@users.noreply.github.com>
2022-12-09 22:03:39 -0300

    Update trigger_procedure.sql

--- server/trigger_procedure.sql
+++ server/trigger_procedure.sql
@@ -4,31 +4,31 @@ create or replace function update_trigger_func()
 returns trigger as $$
 begin
         if(old.cor_luz <> new.cor_luz) then
-            insert into tb_log_cor_luz values(sq_log_cor_luz.nextval,old.id_co,new.cor_luz,current_date);
+            insert into tb_log_cor_luz values(nextval('sq_log_cor_luz'),old.id_co,new.cor_luz,current_date);
         end if;
         
         if(old.periodo_irrigacao <> new.periodo_irrigacao) then
-            insert into tb_log_periodo_irrigacao values(sq_log_periodo_irrigacao.nextval,old.id_co,new.periodo_irrigacao,current_date);
+            insert into tb_log_periodo_irrigacao values(nextval('sq_log_periodo_irrigacao'),old.id_co,new.periodo_irrigacao,current_date);
         end if;
         
         if(old.temperatura <> new.temperatura) then
-            insert into tb_log_temperatura values(sq_log_temperatura.nextval,old.id_co,new.temperatura,current_date);
+            insert into tb_log_temperatura values(nextval('sq_log_temperatura'),old.id_co,new.temperatura,current_date);
         end if;
         
         if(old.umidade <> new.umidade) then
-            insert into tb_log_umidade values(sq_log_umidade.nextval,old.id_co,new.umidade,current_date);
+            insert into tb_log_umidade values(nextval('sq_log_umidade'),old.id_co,new.umidade,current_date);
         end if;
         
         if(old.intensidade_luz <> new.intensidade_luz) then
-            insert into tb_log_intensidade_luz values(sq_log_intensidade_luz.nextval,old.id_co,new.intensidade_luz,current_date);
+            insert into tb_log_intensidade_luz values(nextval('sq_log_intensidade_luz'),old.id_co,new.intensidade_luz,current_date);
         end if;
         
         if(old.intensidade_irrigacao <> new.intensidade_irrigacao) then
-            insert into tb_log_intensidade_irrigacao values(sq_log_intensidade_irrigacao.nextval,old.id_co,new.intensidade_irrigacao,current_date);
+            insert into tb_log_intensidade_irrigacao values(nextval('sq_log_intensidade_irrigacao'),old.id_co,new.intensidade_irrigacao,current_date);
         end if;
         
         if(old.capacidade_tanque_atu <> new.capacidade_tanque_atu) then
-            insert into tb_log_capacidade_tanque_atu values(sq_log_capacidade_tanque_atu.nextval,old.id_co,new.capacidade_tanque_atu,current_date);
+            insert into tb_log_capacidade_tanque_atu values(nextval('sq_log_capacidade_tanque_atu'),old.id_co,new.capacidade_tanque_atu,current_date);
         end if;
     return null;
 end;
